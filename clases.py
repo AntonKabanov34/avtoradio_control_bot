8d465d8e6d94fa96c0d0264a1378d1a84dc30e5d
AntuasK <kabanovradio3@gmail.com>
2023-09-15 16:49:47 +0300

    add config file and add config gitignore

---
+++ clases.py
@@ -0,0 +1,5 @@
+class Audio_file:
+    def __ini__(self, folder_a, folder_b):
+        """Принимает папку ОТКУДА и КУДА"""
+        self.folder_a = folder_a
+        self. folder_b = folder_b
---
+++ config.py
@@ -0,0 +1,12 @@
+# Folders Audio_file
+folder_a = '' # Откуда берем аудиофайлы
+folder_b = '' # Куда копируем аудиофайлы
+
+# data_base
+db_name = 'users.db' #create db name
+
+# bot
+TOKEN = '' # bot_token
+
+#web site parsing news
+web_site = 'https://www.avtoradio.ru/vardata/modules/rss/data/aradionews.rss'
