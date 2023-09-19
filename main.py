90a4958107e8b59fe3c318d5fc3c1ee4fa37d988
AntuasK <kabanovradio3@gmail.com>
2023-09-19 16:29:15 +0300

    add main button, loging users

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.types import ParseMode
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram import types
 
 from config import TOKEN
 from clases import db
-from texts import welcome, users, no_users
+from texts import welcome, users, no_users, on_air, web_site, valute, admin
 
 API_TOKEN = TOKEN  
 
@@ -16,6 +18,21 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
+# Main Keyboard Button
+def main_keyboard_button() -> ReplyKeyboardMarkup:
+    """Выводит командную клавиатуру для верефицированных пользователей"""
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    button_on_air = KeyboardButton(on_air)
+    button_web_site = KeyboardButton(web_site)
+    button_valute = KeyboardButton(valute)
+    button_admin = KeyboardButton(admin)
+
+    markup.add(button_on_air, button_web_site, button_valute)
+    markup.add(button_admin)
+    return markup
+
+
+# Обработчик команд
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     # Проверка на первого пользователя, создание первого админа
@@ -29,8 +46,7 @@ async def send_welcome(message: types.Message):
     elif db.examination_count_user() == False:
         # Логируем пользователя
         if db.loging_users(message.from_user.id):
-            await bot.send_message(message.chat.id, users)
-            # Добавить сюда принтовку основного меню
+            await bot.send_message(message.chat.id, users, reply_markup=main_keyboard_button())
             pass  
         else:
             await bot.send_message(message.chat.id, no_users)
@@ -41,6 +57,51 @@ async def send_welcome(message: types.Message):
         print('Произошла ошибка в обработчике команды START')
         pass
 
+# Обработчик сообщений
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def handle_text_message(message: types.Message):
+    
+    # Обработчик команды "Эфир"
+    if message.text == on_air:
+        if db.loging_users(message.from_user.id):
+            await bot.send_message(message.chat.id, 'Вы запросили доступ к эфиру')
+            pass
+        else:
+            await bot.send_message(message.chat.id, no_users)
+            pass
+    
+    # Обработчик команды "Сайт"
+    elif message.text == web_site:
+        if db.loging_users(message.from_user.id):
+            await bot.send_message(message.chat.id, 'Вы запросили подписку на рассылку сайта')
+            pass
+        else:
+            await bot.send_message(message.chat.id, no_users)
+            pass
+
+    # Обработчик команды "Валюты"
+    elif message.text == valute:
+        if db.loging_users(message.from_user.id):
+            await bot.send_message(message.chat.id, 'Вы запросили доступ к валютам')
+            pass
+        else:
+            await bot.send_message(message.chat.id, no_users)
+            pass
+    
+    # Обработчик команды "Администратор"
+    elif message.text == admin:
+        # Проверка на админа
+        await bot.send_message(message.chat.id, 'Вы запросили доступ к меню администратора')
+        pass
+
+    # На случай если что-то сломается
+    else:
+        print('Что-то произошло в блоке message_handler, обработчике текстовых команд')
+        pass
+
+
+
+        
 
 
 
